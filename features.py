bad224e3c62ca628ba46bea0474c0644dc6254fc
danielesreis <danielesreis@bitbucket.org>
2018-10-09 18:27:44 -0300

    missing: deblurring, remaining features, training script and rotation

--- features.py
+++ features.py
@@ -6,7 +6,8 @@ OFFSET_long 			= 0.05
 OFFSET_Y_apex_stalk 	= 0.15
 OFFSET_X_apex_stalk 	= 0.05
 
-SCALAR_apex_stalk	= 0.05
+HEIGHT_FRACTION			= 0.05
+WIDTH_FRACTION			= 0.05
 
 def channels_mean(img):
 	mean_0 = img[:,:,0].mean()
@@ -45,63 +46,91 @@ def space_diffs(img):
 	return mean_0 - mean_1, mean_0 - mean_2, mean_1 - mean_2
 
 def apex_means(img):
-	width, height, _ 		= img.shape
+	height, width, __ 		= img.shape
 
-	y_i, _ 					= height - round(OFFSET_Y_apex_stalk * height)
-	slice_height 			= round(height * SCALAR_apex_stalk)
-	y_f						= y_i - slice_height
+	y_c						= round(OFFSET_Y_apex_stalk * height)
+	slice_height 			= round(height * HEIGHT_FRACTION / 2)
+	y_f						= y_c + slice_height
+	y_i						= y_c - slice_height
 
-	# get first non zero pixel
-	x_i 					= round(OFFSET_X_apex_stalk * width)
-	x_f						= width - x_i
+	x_0						= first_nonwhite_pixel(img, y_i, 'y')
+	x_1						= first_nonwhite_pixel(img, y_f, 'y')
+	x_i						= x_0 if x_0 > x_i else x_0
 
-	mean_0, mean_1, mean_2 	= channels_mean(img[x_i:x_f, y_i:y_f])
+	x_0						= last_nonwhite_pixel(img, y_i, 'y')
+	x_1						= last_nonwhite_pixel(img, y_f, 'y')
+	x_f						= x_0 if x_0 < x_i else x_0
+
+	mean_0, mean_1, mean_2 	= channels_mean(img[y_i:y_f, x_i:x_f])
 
 	return mean_0, mean_1, mean_2
 
 def equator_means(img):
-	width, height, _ 		= img.shape
+	height, width, __ 		= img.shape
 
-	y_i, _ 					= height - round(0.5 * height)
-	slice_height 			= round(height * SCALAR_apex_stalk)
-	y_f						= y_i - slice_height
+	y_c						= round(height / 2)
+	slice_height 			= round(height * HEIGHT_FRACTION / 2)
+	y_f						= y_c + slice_height
+	y_i						= y_c - slice_height
 
-	x_i 					= round(OFFSET_X_apex_stalk * width)
-	x_f						= width - x_i
+	x_0						= first_nonwhite_pixel(img, y_i, 'y')
+	x_1						= first_nonwhite_pixel(img, y_f, 'y')
+	x_i						= x_0 if x_0 > x_i else x_0
 
-	mean_0, mean_1, mean_2 	= channels_mean(img[x_i:x_f, y_i:y_f])	
+	x_0						= last_nonwhite_pixel(img, y_i, 'y')
+	x_1						= last_nonwhite_pixel(img, y_f, 'y')
+	x_f						= x_0 if x_0 < x_i else x_0
+
+	mean_0, mean_1, mean_2 	= channels_mean(img[y_i:y_f, x_i:x_f])
+
+	return mean_0, mean_1, mean_2
 
 def stalk_means(img):
-	width, height, _ 		= img.shape
+	height, width, __ 		= img.shape
 
-	y_i, _ 					= round(OFFSET_Y_apex_stalk * height)
-	slice_height 			= round(height * SCALAR_apex_stalk)
-	y_f						= y_i + slice_height
+	y_c						= height - round(OFFSET_Y_apex_stalk * height)
+	slice_height 			= round(height * HEIGHT_FRACTION / 2)
+	y_f						= y_c + slice_height
+	y_i						= y_c - slice_height
+	
+	x_0						= first_nonwhite_pixel(img, y_i, 'y')
+	x_1						= first_nonwhite_pixel(img, y_f, 'y')
+	x_i						= x_0 if x_0 > x_i else x_0
 
-	# get first non zero pixel
-	x_i 					= round(OFFSET_X_apex_stalk * width)
-	x_f						= width - x_i
+	x_0						= last_nonwhite_pixel(img, y_i, 'y')
+	x_1						= last_nonwhite_pixel(img, y_f, 'y')
+	x_f						= x_0 if x_0 < x_i else x_0
 
-	mean_0, mean_1, mean_2 	= channels_mean(img[x_i:x_f, y_i:y_f])	
+	mean_0, mean_1, mean_2 	= channels_mean(img[y_i:y_f, x_i:x_f])
 
 	return mean_0, mean_1, mean_2
 
 def longitudinal_gradient(img):
-	grayscale_img 		= cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-	width, height, _ 	= img.shape
+	height, width, __ 	= img.shape
 
-	x_i, x_f			= get_hor_points(img, width)
-	y_i, y_f			= get_vert_points(img, height, OFFSET_long)
+	y_i					= OFFSET_long * height
+	y_f					= height - OFFSET_long * height
+
+	x_c					= round(width / 2)
+	slice_width			= round(width * WIDTH_FRACTION / 2)
+	x_f					= x_c + slice_width
+	x_i					= x_c - slice_width
+
+	long_slice			= img[y_i:y_f,x_i:x_f,:]
+	data 				= np.mean(long_slice, axis=1)
+
+	
+
 
 
 def regions_mean_diffs(img):	
-	apex_R, apex_G, apex_B 			= apex_means(img)
-	equator_R, equator_G, equator_B	= equator_means(img)
-	stalk_R, stalk_G, stalk_B 		= stalk_means(img)
+	apex_0, apex_1, apex_2 			= apex_means(img)
+	equator_0, equator_1, equator_2	= equator_means(img)
+	stalk_0, stalk_1, stalk_2 		= stalk_means(img)
 
-	region_diffs 					= np.array([apex_R - equator_R, equator_R - stalk_R, apex_R - stalk_R \
-												apex_G - equator_G, equator_G - stalk_G, apex_G - stalk_G]\
-												apex_B - equator_B, equator_B - stalk_B, apex_B - stalk_B)
+	region_diffs 					= np.array([apex_0 - equator_0, equator_0 - stalk_0, apex_0 - stalk_0 \
+												apex_1 - equator_1, equator_1 - stalk_1, apex_1 - stalk_1]\
+												apex_2 - equator_2, equator_2 - stalk_2, apex_2 - stalk_2)
 
 	return region_diffs
 
@@ -114,28 +143,23 @@ def correlation_dimension(img):
 def dilation_dimension(img):
 
 
-def estimated_area(binary_img):
-	return len(binary_img[binary_img == 255])
+def estimated_area(gray_img):
+	return len(gray_img[gray_img == 255])
 
-def estimated_diameter(binary_img):
-	area 		= estimated_area(binary_img)
+def estimated_diameter(gray_img):
+	area 		= estimated_area(gray_img)
 	diameter 	= math.sqrt(area * 4 / math.pi)
 
 	return diameter
 
-def get_hor_points(img, width):
-	middle_width 	= OFFSET_long * width
-	hor_offset 		= round((width - middle_width) / 2)
+def first_nonwhite_pixel(img, known_point, axis):
+	data 	= img[known_point,:,0] if axis == 'y' else img[:,known_point,0]
+	i 		= np.where(data != 255)
 
-	x_i 			= hor_offset
-	x_f 			= x_i + middle_width
+	return i[0][0]
 
-	return x_i, x_f
+def last_nonwhite_pixel(img, known_point, axis):
+	data 	= img[known_point,:,0] if axis == 'y' else img[:,known_point,0]
+	i 		= np.where(data != 255)
 
-def get_vert_points(img, height, offset):
-	vert_offset		= round(offset * height)
-
-	y_i				= height - vert_offset
-	y_f				= vert_offset
-
-	return y_i, y_f
+	return i[0][-1]
--- features_ext.py
+++ features_ext.py
@@ -11,9 +11,15 @@ dest_folder		= path + '\\..\\feature_extraction\\'
 feature_names	= [ 'mean_R', 'mean_G', 'mean_B', 'mean_H', 'mean_S', 'mean_V', 'mean_L', 'mean_a', 'mean_b', 'RG_rate', 'RB_rate', \
 					'SH_rate', 'dominant_HSV', 'RG_diff', 'RB_diff', 'GB_diff', 'apex_mean_R', 'apex_mean_G', 'apex_mean_B', \
 					'equator_mean_R', 'equator_mean_G', 'equator_mean_B', 'stalk_mean_R', 'stalk_mean_G', 'stalk_mean_B', \
-					'long_gradient_R', 'long_gradient_G', 'long_gradient_B', 'apex_equator_R_diff', 'equator_stalk_R_diff', \
-					'apex_stalk_R_diff', 'apex_equator_G_diff', 'equator_stalk_G_diff', 'apex_stalk_G_diff', 'apex_equator_B_diff', \
-					'equator_stalk_B_diff', 'apex_stalk_B_diff', 'bcd', 'cd', 'dd', 'area', 'diameter']
+					'apex_mean_H', 'apex_mean_S', 'apex_mean_V', 'equator_mean_H', 'equator_mean_S', 'equator_mean_V', 'stalk_mean_H', \
+					'stalk_mean_S', 'stalk_mean_V', 'apex_mean_L', 'apex_mean_a', 'apex_mean_b', 'equator_mean_L', 'equator_mean_a', \
+					'equator_mean_b', 'stalk_mean_L', 'stalk_mean_a', 'stalk_mean_b', 'long_gradient_R', 'long_gradient_G', \
+					'long_gradient_B', 'apex_equator_R_diff', 'equator_stalk_R_diff', 'apex_stalk_R_diff', 'apex_equator_G_diff', \
+					'equator_stalk_G_diff', 'apex_stalk_G_diff', 'apex_equator_B_diff', 'equator_stalk_B_diff', 'apex_stalk_B_diff', \
+					'apex_equator_H_diff', 'equator_stalk_H_diff', 'apex_stalk_H_diff', 'apex_equator_S_diff', 'equator_stalk_S_diff', \
+					'apex_stalk_S_diff', 'apex_equator_V_diff', 'equator_stalk_V_diff', 'apex_stalk_V_diff', 'apex_equator_L_diff', \
+					'equator_stalk_L_diff', 'apex_stalk_L_diff', 'apex_equator_a_diff', 'equator_stalk_a_diff', 'apex_stalk_a_diff', \
+					'apex_equator_b_diff', 'equator_stalk_b_diff', 'apex_stalk_b_diff', 'bcd', 'cd', 'dd', 'area', 'diameter']
 
 for seg_folder in folders:
 	img_names 	= os.listdir(path + seg_folder)
@@ -22,8 +28,7 @@ for seg_folder in folders:
 	for img_name in img_names:
 		BGR_img 	= cv2.imread(img_name)
 		gray_img 	= cv2.cvtColor(BGR_img, cv2.COLOR_BGR2GRAY)
-		binary_img 	= get_inverse_mask(gray_img)
-		
+				
 		RGB_img 	= BGR_img[:,:,::-1]
 		HSV_img 	= cv2.cvtColor(RGB_img, cv2.COLOR_RGB2HSV)
 		#check this
@@ -33,29 +38,51 @@ for seg_folder in folders:
 		mean_R, mean_G, mean_B 								= ft.channels_mean(RGB_img)
 		mean_H, mean_S, mean_B 								= ft.channels_mean(HSV_img)
 		mean_L, mean_a, mean_b 								= ft.channels_mean(Lab_img)
+
 		RG_rate, RB_rate, __ 								= ft.space_rates(RGB_img)
-		# it's H/S actually
 		SH_rate, __, __ 									= ft.space_rates(HSV_img)
+
 		dominant_HSV										= ft.dominant_HSV_color(HSV_img)
+
 		RG_diff, RB_diff, GB_diff							= ft.space_diffs(RGB_img)
+
 		apex_mean_R, apex_mean_G, apex_mean_B 				= ft.apex_means(RGB_img)
 		equator_mean_R, equator_mean_G, equator_mean_B 		= ft.equator_means(RGB_img)
 		stalk_mean_R, stalk_mean_G, stalk_mean_B 			= ft.stalk_means(RGB_img)
+		apex_mean_H, apex_mean_S, apex_mean_V 				= ft.apex_means(HSV_img)
+		equator_mean_H, equator_mean_S, equator_mean_V 		= ft.equator_means(HSV_img)
+		stalk_mean_H, stalk_mean_S, stalk_mean_V 			= ft.stalk_means(HSV_img)
+		apex_mean_L, apex_mean_a, apex_mean_b 				= ft.apex_means(Lab_img)
+		equator_mean_L, equator_mean_a, equator_mean_b 		= ft.equator_means(Lab_img)
+		stalk_mean_L, stalk_mean_a, stalk_mean_b 			= ft.stalk_means(Lab_img)
+		
 		long_gradient_R, long_gradient_G, long_gradient_B 	= ft.longitudinal_gradient(RGB_img)
-		regions_diffs										= ft.regions_mean_diffs(RGB_img)
+
+		regions_diffs_RGB									= ft.regions_mean_diffs(RGB_img)
+		regions_diffs_HSV									= ft.regions_mean_diffs(HSV_img)
+		regions_diffs_Lab									= ft.regions_mean_diffs(Lab_img)
+
 		bcd  												= ft.box_counting_dimension(RGB_img)
 		cd  												= ft.correlation_dimension(RGB_img)
 		dd  												= ft.dilation_dimension(RGB_img)
-		# area is the same as number of pixels
-		area												= ft.estimated_area(binary_img)
-		diameter											= ft.estimated_diameter(binary_img)
+
+		area												= ft.estimated_area(gray_img)
+		diameter											= ft.estimated_diameter(gray_img)
 
 		feature_values	= [ mean_R, mean_G, mean_B, mean_H, mean_S, mean_V, mean_L, mean_a, mean_b, RG_rate, RB_rate, \
 							SH_rate, dominant_HSV, RG_diff, RB_diff, GB_diff, apex_mean_R, apex_mean_G, apex_mean_B, \
 							equator_mean_R, equator_mean_G, equator_mean_B, stalk_mean_R, stalk_mean_G, stalk_mean_B, \
-							long_gradient_R, long_gradient_G, long_gradient_B, regions_diffs[0], regions_diffs[1], \
-							regions_diffs[2], regions_diffs[3], regions_diffs[4], regions_diffs[5], regions_diffs[6], \
-							regions_diffs[7], regions_diffs[8], bcd, cd, dd, n_pixels, area, diameter]
+							apex_mean_H, apex_mean_S, apex_mean_V, equator_mean_H, equator_mean_S, equator_mean_V, stalk_mean_H,\
+							stalk_mean_S, stalk_mean_V, apex_mean_L, apex_mean_a, apex_mean_b, equator_mean_L, equator_mean_a, \
+							equator_mean_b, stalk_mean_L, stalk_mean_a, stalk_mean_b, long_gradient_R, long_gradient_G, long_gradient_B, \
+							regions_diffs_RGB[0], regions_diffs_RGB[1], regions_diffs_RGB[2], regions_diffs_RGB[3], \
+							regions_diffs_RGB[4], regions_diffs_RGB[5], regions_diffs_RGB[6], regions_diffs_RGB[7], \
+							regions_diffs_RGB[8], regions_diffs_HSV[0], regions_diffs_HSV[1], regions_diffs_HSV[2], \
+							regions_diffs_HSV[3], regions_diffs_HSV[4], regions_diffs_HSV[5], regions_diffs_HSV[6], \
+							regions_diffs_HSV[7], regions_diffs_HSV[8], regions_diffs_Lab[0], regions_diffs_Lab[1], \
+							regions_diffs_Lab[2], regions_diffs_Lab[3], regions_diffs_Lab[4], regions_diffs_Lab[5], \
+							regions_diffs_Lab[6], regions_diffs_Lab[7], regions_diffs_Lab[8], \
+							bcd, cd, dd, n_pixels, area, diameter]
 
 		feature_row = pd.Series(data = feature_values, index = feature_names, name = img_name)
 
