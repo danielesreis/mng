f28444fd9ecfb0f5b51bc6686542603c469fbce0
danielesreis <danielesreis@bitbucket.org>
2018-10-16 13:08:29 -0300

    working on correlation dimension | missing: deblurring, fractal variables for intensities and cv size

--- MNG_Features.py
+++ MNG_Features.py
@@ -39,7 +39,7 @@ class MNG_Features():
 
 	def extract_features(self, BGR_img, img_name):
 		
-		grayscale_img 	= cv2.cvtColor(BGR_img, cv2.COLOR_BGR2GRAY)
+		gray_img 	= cv2.cvtColor(BGR_img, cv2.COLOR_BGR2GRAY)
 				
 		RGB_img 		= BGR_img[:,:,::-1]
 		HSV_img 		= cv2.cvtColor(RGB_img, cv2.COLOR_RGB2HSV)
@@ -76,12 +76,12 @@ class MNG_Features():
 		n_regions_means										= self.regions_mean(RGB_img, n_regions)
 		n_regions_means_diffs								= self.regions_mean_diffs(RGB_img, n_regions)
 
-		bcd  												= self.box_counting_dimension(grayscale_img)
-		cd  												= self.correlation_dimension(grayscale_img)
-		dd  												= self.dilation_dimension(grayscale_img)
+		bcd  												= self.box_counting_dimension(gray_img)
+		cd  												= self.correlation_dimension(gray_img)
+		dd  												= self.dilation_dimension(gray_img)
 
-		area												= self.estimated_area(grayscale_img)
-		diameter											= self.estimated_diameter(grayscale_img)
+		area												= self.estimated_area(gray_img)
+		diameter											= self.estimated_diameter(gray_img)
 
 		feature_values	= [ mean_R, mean_G, mean_B, mean_H, mean_S, mean_V, mean_L, mean_a, mean_b, RG_rate, RB_rate, \
 							SH_rate, dominant_HSV, RG_diff, RB_diff, GB_diff, apex_mean_R, apex_mean_G, apex_mean_B, \
@@ -277,33 +277,36 @@ class MNG_Features():
 
 		return region_diffs
 
-	def box_counting_dimension(self, grayscale_img, n = 20):
+	def box_counting_dimension(self, gray_img):
 
-		def box_count(grayscale_img, k):
+		def get_box_sizes(gray_img):
+		    min_dim = min(gray_img.shape)
+		    n 		= int(np.floor(np.log(min_dim)/np.log(2)))
+		    sizes 	= 2**np.arange(n, 1, -1)
+		    return sizes
+
+		def box_count(gray_img, k):
 	        img_sum = np.add.reduceat(
-	            np.add.reduceat(grayscale_img, np.arange(0, grayscale_img.shape[0], k), axis=0),
-	                               np.arange(0, grayscale_img.shape[1], k), axis=1)
+	            np.add.reduceat(gray_img, np.arange(0, gray_img.shape[0], k), axis=0),
+	                               np.arange(0, gray_img.shape[1], k), axis=1)
 
 	        return len(np.where((img_sum > 0) & (img_sum < 255))[0])
 
-	    grayscale_img[grayscale_img != 255] = 0
+	    gray_img[gray_img != 255] 	= 0
+	    
+	    sizes 	= get_box_sizes(gray_img)
+	    counts 	= []
 
-	    # min_dim 	= min(grayscale_img.shape)
-	    # n 			= 2**np.floor(np.log(min_dim)/np.log(2))
-	    # n 			= int(np.log(n)/np.log(2))
-
-	    sizes 		= 2**np.arange(n, 1, -1)
-	    counts 		= []
 	    for size in sizes:
-	        counts.append(box_count(grayscale_img, size))
+	        counts.append(box_count(gray_img, size))
 
 	    coeffs = np.polyfit(np.log(sizes), np.log(counts), 1)
 	    return -coeffs[0]
 
-	def correlation_dimension(self, grayscale_img):
-		grayscale_img[grayscale_img != 255] = 0
+	def correlation_dimension(self, gray_img):
+		gray_img[gray_img != 255] = 0
 
-		height, width, __ 	= grayscale_img.shape
+		height, width, __ 	= gray_img.shape
 		n_pixels 			= height * width
 
 		def heaviside_func(threshold, value):
@@ -327,7 +330,7 @@ class MNG_Features():
 					y_i = y_ind(i+1)
 					x_j = x_ind(j+1)
 					y_j = y_ind(j+1)
-					sum_pixel = sum_pixel + heaviside_func(size, grayscale_img[y_i,x_i]-grayscale_img[y_j,x_j])
+					sum_pixel = sum_pixel + heaviside_func(size, gray_img[y_i,x_i]-gray_img[y_j,x_j])
 
 			sum_pixel[num_it-1] = sum_pixel[num_it-1]/(n_pixels*n_pixels)
 			num_it 				= num_it + 1
@@ -336,31 +339,58 @@ class MNG_Features():
 
 		return -coeffs[0]
 
-	def dilation_dimension(self, grayscale_img):
-		grayscale_img[grayscale_img != 255] = 0
+	def dilation_dimension(self, gray_img):
+		
+		def invert_colors(gray_img):
+			gray_img[gray_img==255] = 0
+			gray_img[gray_img!=0] = 255
+			return gray_img
 
-		kernel = np.array([	[0, 0, 0, 0, 1, 0, 0, 0, 0], \
-							[0, 0, 0, 1, 1, 1, 0, 0, 0], \
-							[0, 0, 1, 1, 1, 1, 1, 0, 0], \
-							[0, 0, 0, 1, 1, 1, 0, 0, 0], \
-							[0, 0, 0, 0, 1, 0, 0, 0, 0]])
+		def get_radius_array(gray_img, h, w)
+			min_dim = min(h, w)
+			n = int(np.floor(np.log(min_dim)/np.log(2)))
 
-		# kernels = 
-		# diameters = 
-		results	= [cv2.filter2D(grayscale_img, -1, kernel) for kernel in kernels]
-		sums 	= [sum(sum(result)) for result in results]
-		L 		= [summ/diameter for summ, diameter in zip(sums, diameters)]
+			radius = np.arange(n, 0, -1)
+			radius = radius*radius
+			return radius
+
+		def fill_image(gray_img, border, h, w):
+			new_img = np.zeros((h+border*2,w+border*2), dtype='uint8')
+			new_img[border:border+h,border:border+w] = gray_img
+			return new_img
+
+		def compute_sums(radius, k_size, gray_img):
+			sums = []
+			for r in radius:
+	    		k = np.full((k_size,k_size), 0, dtype='uint8')
+	    		new_k = cv2.circle(k,(int(k_size/2),int(k_size/2)), r, 1, -1)
+	    		res = cv2.filter2D(gray_img, -1, new_k)
+	    		sums.append(sum(sum(res)))
+
+		h, w 	= gray_img.shape
+		img 	= invert_colors(gray_img)
+
+		radius 			= get_radius_array(gray_img, h, w)
+    	diameters 		= radius*2
+		max_diameter 	= max(diameters)
+		
+		k_size 		= max_diameter-max_diameter%100+100
+		border 		= max_diameter+50
+		gray_img 	= fill_image(gray_img, border, h, w)
+
+		sums = compute_sums(radius, k_size, gray_img)
+
+		L = [s/d for s, d in zip(sums, diameters)]
 
 		coeffs = np.polyfit(np.log(L), np.log(1/diameters), 1)
-
 		return -coeffs[0]
 
-	def estimated_area(self, grayscale_img):
-		return len(grayscale_img[grayscale_img == 255])
+	def estimated_area(self, gray_img):
+		return len(gray_img[gray_img == 255])
 
-	def estimated_diameter(self, grayscale_img):
-		area 		= self.estimated_area(grayscale_img)
-		diameter 	= math.sqrt(area * 4 / math.pi)
+	def estimated_diameter(self, gray_img):
+		area 		= self.estimated_area(gray_img)
+		diameter 	= math.sqrt(area*4/math.pi)
 
 		return diameter
 
