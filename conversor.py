fa8c6b72041a34d019d733d0a09d77e257f72212
danielesreis <danielesreis@bitbucket.org>
2018-11-11 16:42:38 -0200

    complete tss files added and cr2-to-jpg converter added

---
+++ conversor.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+path = os.getcwd()
+# dest = 
+files = os.listdir(path)
+
+# raw2jpg.exe -i <input file> -o <output folder>
+# you must run it from C:\Program Files (x86)\Easy2Convert Software\RAW to JPG
+
+for file in files:
+	subprocess.run(["raw2jpg.exe", "-i", file, "-o", dest])
